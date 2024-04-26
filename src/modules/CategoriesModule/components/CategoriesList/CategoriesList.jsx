7d30ab577ca69af1227ad5fa804d08cf820519c0
sondos <sondosmohamedee092@gmail.com>
2024-04-26 04:00:22 +0300

    first commit

---
+++ src/modules/CategoriesModule/components/CategoriesList/CategoriesList.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../../../SharedModules/components/Header/Header'
+import header from '../../../../assets/images/header.png'
+
+export default function CategoriesList() {
+  return (
+    <>
+    <Header 
+      title={'Categories Item'}
+      description={'You can now add your items that any user can order it from the Application and you can edit'} 
+      imgUrl={header} 
+    /> 
+
+
+
+    </>
+  )
+}
